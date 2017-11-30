3f9f71da0668061e061b4152c6c53c91b13d2c34
Minh Dao T <minh.dao@asiantech.vn>
2017-11-30 14:53:51 +0700

    register user

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -1,26 +1,46 @@
 <?php
 
-    namespace App\Controllers;
+namespace App\Controllers;
 
-    use App\Models\User;
+use App\Models\User;
 
-    class UsersController extends Controller
+class UsersController extends Controller
+{
+    public function login()
     {
-        public function show($id)
-        {
-            $data['id'] = $id;
-            view('users.show', $data);
-        }
-        public function test()
-        {
-            view('users.test');
-        }
-        public function getAll()
-        {
-            $user = new User();
-            $arUser = $user->all();
-            echo "<pre>";
-            print_r($arUser);
-            echo "</pre>";
-        }
+        view('users.login');
     }
+    public function register()
+    {
+        $arUser = array();
+        $arUser['username'] = $_POST['username'];
+        $arUser['fullname'] = $_POST['fullname'];
+        $arUser['password'] = $_POST['password'];
+
+        $user = new User();
+        $result = $user->addUser($arUser);
+        if ($result) {
+            $data['msg'] = "Success!";
+        } else {
+            $data['msg'] = "Fail!";
+        }
+        view('home.register',$data);
+    }
+    public function show($id)
+    {
+        $data['id'] = $id;
+        view('users.show', $data);
+    }
+    public function test()
+    {
+        view('users.test');
+    }
+    public function getAll()
+    {
+        $user = new User();
+        $arUser = $user->all();
+        echo "<pre>";
+        print_r($arUser);
+        echo "</pre>";
+    }
+}
