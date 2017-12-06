e6c516d13c6147796e17b4ecb333601406dcc3c2
Minh Dao T <minh.dao@asiantech.vn>
2017-12-06 15:06:34 +0700

    done pagination

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,44 @@ class Post extends Model
 
 		return $stmt->fetchAll();
 	}
+	public function pagination($limit) {
+		//total records
+		$total_records = $this->totalRecord();
+		//current page
+		$current_page  = isset($_GET['page']) ? $_GET['page'] : 1;
+		//total page
+		$total_page    = ceil($total_records / $limit);
+		//validate page exist
+		if ($current_page > $total_page){
+			$current_page = $total_page;
+		}
+		else if ($current_page < 1){
+			$current_page = 1;
+		}
+		//start value
+		$start = ($current_page - 1) * $limit;
+		//get list post with pagination
+		$sql  = "SELECT id,name,preview_text,user_id,date_create FROM posts ORDER BY id DESC LIMIT $start, $limit";
+		$stmt = static::$db->prepare($sql);
+		$stmt->execute();
+
+		return $stmt->fetchAll();
+	}
+
+	public function totalRecord() {
+		$sql  = "SELECT count(id) as total from posts";
+		$stmt = static::$db->prepare($sql);
+		$stmt->execute();
+		$row  = $stmt->fetch();
+		return $row['total'];
+	}
+
+	public function totalPage($limit) {
+		$total_records = $this->totalRecord();
+		//return total pages
+		return ceil($total_records / $limit);
+	}
+
 	public function addPost($arPost)
 	{
 		$sql = "INSERT INTO posts(name, preview_text, detail_text, user_id, date_create) VALUES (:name,:preview_text,:detail_text,:user_id,:date_create)";
